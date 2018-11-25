28ccf45ad7f20321ea79dcb197c4d4abcb3c2d4f
Nath Paiva <hello@nathpaiva.com.br>
2018-11-25 20:57:59 -0200

    Creaete a comment component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,8 +1,32 @@
 import React, { PureComponent } from 'react';
 
 class Comments extends PureComponent {
+  renderComments(comment, i) {
+		console.log("​Comments -> renderComments -> i", i)
+		console.log("​Comments -> renderComments -> comment", comment)
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>{comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }
+
   render() {
-    return (<div className="comment">Hello</div>);
+    const { postComments } = this.props;
+
+    return (
+      <div className="comments">
+        {postComments.map(this.renderComments)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" name="author" id="author" ref="author" placeholder="author" />
+          <input type="text" name="comment" id="comment" ref="comment" placeholder="comment" />
+          <input type="submit" hidden name="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
--- client/components/Single.js
+++ client/components/Single.js
@@ -5,14 +5,17 @@ import Comments from './Comments';
 class Single extends PureComponent {
 
   render() {
-    const { posts, match } = this.props;
-    const i = posts.findIndex((post) => post.code == match.params.postId);
+    const { posts, match, comments } = this.props;
+    const { postId } = match.params;
+    const i = posts.findIndex((post) => post.code == postId);
     const post = posts[i];
+    const postComments = comments[postId] || [];
+		console.log("​Single -> render -> postComments", postComments)
 
     return (
       <div className="single-photo">
         <Photo key={post.code} i={i} {...this.props} post={post} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     );
   }
