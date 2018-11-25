aa0f96a1e344d79091ed47f494e55360313f3069
Nath Paiva <hello@nathpaiva.com.br>
2018-11-25 12:14:33 -0200

    Create a photo component

--- client/components/Main.js
+++ client/components/Main.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 import PhotoGrid from './PhotoGrid';
 import Single from './Single';
 
-const Main = ({ history, posts }) => (
+const Main = ({ history, posts, comments }) => (
   <main>
     <h1><Link to="/">Reduxtagram</Link></h1>
     <Switch>
-      <Route path="/" exact render={() => <PhotoGrid posts={posts} />} />
+      <Route path="/" exact render={() => <PhotoGrid posts={posts} comments={comments} />} />
       <Route path="/view/:postId" component={Single} />
     </Switch>
   </main>
